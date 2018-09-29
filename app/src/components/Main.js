482cd7a9786ed5782fa6c23bf5954d7f71665a5a
Marco Pineda <mapineda1222@gmail.com>
2018-09-29 16:20:43 -0500

    Adds player and buttons

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -8,9 +8,33 @@ let yeomanImage = require('../images/yeoman.png');
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
+      <div className="index container">
         <img src={yeomanImage} alt="Yeoman Generator" />
-        <MapComponent />
+        <div className="row">
+          <div className="col-lg-9">
+            <MapComponent />
+          </div>
+        </div>
+        <div className="mt-3">
+          <div id='permissions'>
+            <div className="d-flex flex-row">
+              <p className="mr-3">Record:</p>
+              <form>
+                <label className="ml-3 mr-2">Audio</label>
+                <input type="radio" name="media" value="audio" defaultChecked id="mediaAudio"/>
+              </form>
+            </div>
+            <button className="btn btn-primary"  id='optInBtn'>Request Stream</button>
+          </div>
+          <div id='btns'>
+            <button  className="btn btn-success" id='start'>Start</button>
+            <button  className="btn btn-danger" id='stop'>Stop</button>
+          </div>
+          <div>
+            <ul  className="list-unstyled" id='ul'></ul>
+          </div>
+
+        </div>
       </div>
     );
   }
