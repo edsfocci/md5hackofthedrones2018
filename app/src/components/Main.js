78f123bbbed13c09a1f5d486d177ba0d4e56a552
Marco Pineda <mapineda1222@gmail.com>
2018-09-29 12:50:02 -0500

    Adds start recording and stop recording buttons

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,16 +1,28 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
-
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+import './getAudio';
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Monkey Jelly</h1>
+        </header>
+          <div>
+            <div>
+              <h2>Audio record and playback</h2>
+              <p>
+                <button id="startRecord" className="btn ml-3">start</button>
+                <button id="stopRecord" className="btn" disabled>stop</button>
+              </p>
+              <p>
+                <audio id="recordedAudio" />
+                <a id="audioDownload" />
+              </p>
+            </div>
+        </div>
       </div>
     );
   }
