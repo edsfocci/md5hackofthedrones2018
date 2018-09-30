1b545f902be7fa01f3ca49f0520eee531bfead92
Marco Pineda <mapineda1222@gmail.com>
2018-09-29 23:30:32 -0500

    Removes geo loggin on front end

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -15,25 +15,14 @@ class AppComponent extends React.Component {
             <MapComponent />
           </div>
         </div>
-        <div className="mt-3">
-          <div id='permissions'>
-            <div className="d-flex flex-row">
-              <p className="mr-3">Record:</p>
-              <form>
-                <label className="ml-3 mr-2">Audio</label>
-                <input type="radio" name="media" value="audio" defaultChecked id="mediaAudio"/>
-              </form>
-            </div>
-            <button className="btn btn-primary"  id='optInBtn'>Request Stream</button>
-          </div>
-          <div id='btns'>
-            <button  className="btn btn-success" id='start'>Start</button>
-            <button  className="btn btn-danger" id='stop'>Stop</button>
-          </div>
-          <div>
-            <ul  className="list-unstyled" id='ul'></ul>
-          </div>
+        <div>
+          <button id="start-btn" className="btn">Start recording</button>
+          <button id="stop-btn" className="btn" disabled>Stop recording</button>
 
+          <h2>Stored Recordings</h2>
+          <ul id="recordingslist"></ul>
+
+          <span id="result"></span>
         </div>
       </div>
     );
